4aec04cf72a3a5ffaadc9a19f9d8ae41ff23ca2c
Matías Games <gabrielgamesmorgani@gmail.com>
2025-05-09 18:31:41 -0300

    cambios para resolver problemas

--- ecommerceBackend/src/main/java/org/example/ecommercebackend/controllers/Usuario/UsuarioDireccionController.java
+++ ecommerceBackend/src/main/java/org/example/ecommercebackend/controllers/Usuario/UsuarioDireccionController.java
@@ -3,11 +3,6 @@ package org.example.ecommercebackend.controllers.Usuario;
 import org.example.ecommercebackend.controllers.BaseController;
 import org.example.ecommercebackend.entities.Usuario.UsuarioDireccion;
 import org.example.ecommercebackend.services.Usuario.UsuarioDireccionService;
-<<<<<<< HEAD
-import org.example.ecommercebackend.services.Usuario.UsuarioService;
-import org.example.ecommercebackend.services.Usuario.UsuarioDireccionService;
-=======
->>>>>>> a25746ff0f44d019280bbef5eb6e1f7b2fe29eea
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
